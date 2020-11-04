6e426c71f6c41cf12ca75548bf52e8553bc8db48
BuildTools <unconfigured@null.spigotmc.org>
2020-11-04 16:28:36 -0700

    fixed john npc breaking in the nether (2)

--- src/me/shufy/john/randomevents/npc/events/JohnNpcChase.java
+++ src/me/shufy/john/randomevents/npc/events/JohnNpcChase.java
@@ -19,10 +19,13 @@ public class JohnNpcChase extends RandomEvent implements JohnableRandomEvent {
         super(eventName, players, location, chance);
     }
     Npc john;
+    boolean shouldNotExecute = false;
     @Override
     public void setup(Location location) {
       //  Bukkit.getLogger().log(Level.INFO, "Running JohnNpcChase");
-        if (!location.getWorld().getEnvironment().equals(World.Environment.NETHER)) {
+        if (location.getWorld().getEnvironment() != null)
+        shouldNotExecute = (location.getWorld().getEnvironment().equals(World.Environment.NETHER);
+        if (!shouldNotExecute) {
             john = new Npc("John", JOHN_UUID.toString(), location);
             setRunForTicks(randomInt(200, 500));
             setEventWorld(getWorldWithMostPlayers());
@@ -33,19 +36,22 @@ public class JohnNpcChase extends RandomEvent implements JohnableRandomEvent {
 
     @Override
     public void execution(Collection<Player> playersInvolved) {
-        john.summon();
-        if (john == null) {
-            Bukkit.getLogger().log(Level.SEVERE, "[JohnNpcChase] JOHN IS NULL! Cannot execute the JohnableRandomEvent \"JohnNpcChase\"");
-            Bukkit.getLogger().log(Level.SEVERE, "[JohnNpcChase] Event must halt.");
-            return;
-        }
-        for (Player player : playersInvolved) {
-            player.playSound(player.getLocation(), randomSoundWhoContains("ambient"), 1f, ThreadLocalRandom.current().nextFloat());
+        if (!shouldNotExecute) {
+            john.summon();
+            if (john == null) {
+                Bukkit.getLogger().log(Level.SEVERE, "[JohnNpcChase] JOHN IS NULL! Cannot execute the JohnableRandomEvent \"JohnNpcChase\"");
+                Bukkit.getLogger().log(Level.SEVERE, "[JohnNpcChase] Event must halt.");
+                return;
+            }
+            for (Player player : playersInvolved) {
+                player.playSound(player.getLocation(), randomSoundWhoContains("ambient"), 1f, ThreadLocalRandom.current().nextFloat());
+            }
         }
     }
 
     @Override
     public void cleanup(Location location, Collection<Player> playersInvolved) {
-        john.destroy(false);
+        if (john != null)
+            john.destroy(false);
     }
 }
